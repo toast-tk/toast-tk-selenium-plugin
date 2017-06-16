ce90231cdaae13b20ec47d2d8d4ed7a00025c449
Sallah Kokaina <sallah.kokaina@gmail.com>
2017-06-16 10:24:53 +0000

    fix(build): update publish settings

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo "Deploy for tag: $TRAVIS_TAG - branch: $TRAVIS_BRANCH"
+
 if [ $TRAVIS_PULL_REQUEST == "false" ] && [ ${TRAVIS_BRANCH} = 'snapshot' ]; then
-    mvn deploy --settings ./settings.xml -DskipTests=true -Pdeploy;
+    mvn clean deploy --settings ./settings.xml -DskipTests=true -Pdeploy;
     exit $?;
+elif [ ! -z "$TRAVIS_TAG" ]; then
+	echo 'Deploying release tag to OSS'
+ 	mvn --settings ./settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG;
+ 	mvn clean install -DskipTests=true --settings ./settings.xml;
+	mvn --batch-mode deploy --settings ./settings.xml -DskipTests=true -Pdeploy;
+	exit $?;
 fi
