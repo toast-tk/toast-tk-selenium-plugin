cf7850369b0f5ebb96429af9d6832481bcc58a7c
Sallah Kokaina <sallah.kokaina@gmail.com>
2017-06-28 14:27:23 +0000

    build(0.1.5): debug settings

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'snapshot' ]]; then
       echo 'Checking Quality Gates against sonar'
-      mvn help:effective-settings --settings ./settings.xml -DshowPasswords=true;
       mvn --batch-mode clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_LOGIN} -Dsonar.organization=${SONAR_ORGANIZATION} --settings ./settings.xml;
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
       echo 'Build and analyze pull request'
